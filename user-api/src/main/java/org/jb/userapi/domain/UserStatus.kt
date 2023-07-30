7618676f37969b2d2aa1b05ba1828af1cf63dd3a
João Oliveira <jb-oliveira@github.com>
2023-07-30 17:20:34 -0300

    Add user repository

--- user-api/src/main/java/org/jb/userapi/domain/UserStatus.kt
+++ user-api/src/main/java/org/jb/userapi/domain/UserStatus.kt
@@ -2,5 +2,12 @@ package org.jb.userapi.domain
 
 enum class UserStatus {
     Active,
-    Inactive
+    Inactive;
+
+    companion object {
+        fun fromString(status: String): UserStatus {
+            return values().find { it.name.equals(status, ignoreCase = true) }
+                ?: throw IllegalArgumentException("Invalid status: $status")
+        }
+    }
 }
