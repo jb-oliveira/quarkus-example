c9188bb07226d83b107444c5fcbdabe2d9a0d2de
João Oliveira <jb-oliveira@github.com>
2023-07-30 16:29:37 -0300

    Inser user use case.

--- user-api/src/main/java/org/jb/userapi/domain/Cpf.kt
+++ user-api/src/main/java/org/jb/userapi/domain/Cpf.kt
@@ -1,4 +1,28 @@
 package org.jb.userapi.domain
 
 class Cpf {
+    lateinit var value: String;
+
+    fun validate(cpf: String): Boolean {
+        val cpfOnlyDigits = cpf.replace("[^0-9]".toRegex(), "")
+        if (cpfOnlyDigits.length != 11) {
+            return false
+        }
+        if (cpfOnlyDigits.matches("(\\d)\\1{10}".toRegex())) {
+            return false
+        }
+        val d1 = checkDigit(cpfOnlyDigits.substring(0, 9))
+        val d2 = checkDigit(cpfOnlyDigits.substring(0, 9) + d1)
+        return cpfOnlyDigits == cpfOnlyDigits.substring(0, 9) + d1 + d2
+    }
+
+    private fun checkDigit(part: String): Int {
+        var sum = 0
+        var height = part.length + 1
+        for (i in 0 until part.length) {
+            sum += part.substring(i, i + 1).toInt() * height--
+        }
+        val modulus = sum % 11
+        return if (modulus < 2) 0 else 11 - modulus
+    }
 }
