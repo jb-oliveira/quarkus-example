96a1f053c6124fd2f511f5980cfe3a919484fe32
João Oliveira <jb-oliveira@github.com>
2023-07-30 16:17:38 -0300

    Inser user use case.

---
+++ user-api/src/main/java/org/jb/userapi/application/usecase/InsertUser.kt
@@ -0,0 +1,17 @@
+package org.jb.userapi.application.usecase
+
+import jakarta.enterprise.context.ApplicationScoped
+import jakarta.inject.Inject
+import org.jb.userapi.application.repository.UserRepository
+import org.jb.userapi.domain.User
+
+
+@ApplicationScoped
+class InsertUser {
+    @Inject
+    lateinit var repository: UserRepository
+    fun execute(input: User): User {
+        repository.insert(input)
+        return input
+    }
+}
---
+++ user-api/src/main/java/org/jb/userapi/domain/Cpf.kt
@@ -0,0 +1,4 @@
+package org.jb.userapi.domain
+
+class Cpf {
+}
