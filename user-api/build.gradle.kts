9b8897b298db2de967d9ca27086ef5ba94111ba6
João Oliveira <jb-oliveira@github.com>
2023-07-31 20:32:30 -0300

    adding yml configuyration

--- user-api/build.gradle.kts
+++ user-api/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation("io.quarkus:quarkus-arc")
     implementation("io.quarkus:quarkus-hibernate-orm")
     implementation("io.quarkus:quarkus-resteasy-reactive")
+    implementation("io.quarkus:quarkus-config-yaml")
     testImplementation("io.quarkus:quarkus-junit5")
     testImplementation("io.rest-assured:rest-assured")
 }
